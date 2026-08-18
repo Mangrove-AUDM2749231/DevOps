66b9136dc2352ced4f89ba30af437e9178df025c
Mangrove-AUDM2749231 <2893495210@example.com>
2026-08-18 21:51:51 +0800

    test: update app message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, DevOps! 部署成功！'
+    return 'Hello, CI/CD! 离线导入测试成功！🚀'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
